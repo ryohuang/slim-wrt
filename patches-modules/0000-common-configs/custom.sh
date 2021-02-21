52c8cb4226323247a2b23dda1929ae364a327179
ryohuang <anyisdad@icloud.com>
2021-02-21 14:42:07 +0800

    slim: change method of custom feed

--- patches-modules/0000-common-configs/custom.sh
+++ patches-modules/0000-common-configs/custom.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# append slimapps
+if grep -q "slim" $SLIM_CFG_WORK_PATH/feeds.conf.default
+then
+    echo "Already exit slimapps"
+else
+    cat <<EOT >> $SLIM_CFG_WORK_PATH/feeds.conf.default
+
+src-cpy slim  ../slimapps
+
+EOT
+fi
+
 # all the custom scripts will run in their own path.
 cd $SLIM_CFG_WORK_PATH
 ./scripts/feeds update slim
