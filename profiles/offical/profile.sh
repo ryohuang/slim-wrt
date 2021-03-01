63cc889cfadd2c0064a5ebbcc06c5acfab75e855
ryohuang <anyisdad@icloud.com>
2021-03-02 07:59:07 +0800

    Pure official build config to check the vermagic.

--- profiles/offical/profile.sh
+++ profiles/offical/profile.sh
@@ -4,13 +4,12 @@ SLIM_CFG_TARGET="x86"
 SLIM_CFG_ARCH="64"
 
 SLIM_CFG_EXCLUDE_PATCH=(\
+                        0000-common-configs \
+                        0001-fullcone-nat \
                         0002-upnp \
+                        0003-nf-conntrack-max \
+                        0004-workaround-for-realtime-connection-status-crash \
+                        0005-slim-localization-configuration \
+                        0006-fake-vermagic \
                         )
 
-echo ${SLIM_CFG_EXCLUDE_PATCH[*]}
-
-# You can add more exclude patches as following lines.
-# SLIM_CFG_EXCLUDE_PATCH=(\
-#                         0002-upnp \
-#                         0003-nf-conntrack-max \
-#                         )
