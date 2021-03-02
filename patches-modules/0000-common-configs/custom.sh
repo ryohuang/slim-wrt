a9da3da4cd6c5a20fca9fa337e062ea9e6873ea2
ryohuang <anyisdad@icloud.com>
2021-03-02 20:50:13 +0800

    [slim] Add diskman by lisaac

--- patches-modules/0000-common-configs/custom.sh
+++ patches-modules/0000-common-configs/custom.sh
@@ -8,6 +8,7 @@ else
     cat <<EOT >> $SLIM_CFG_WORK_PATH/feeds.conf.default
 
 src-cpy slim  ../slimapps
+src-git diskman https://github.com/lisaac/luci-app-diskman.git^975859fb7030ded885287c312a8127333f656930
 
 EOT
 fi
@@ -16,4 +17,8 @@ fi
 cd $SLIM_CFG_WORK_PATH
 ./scripts/feeds update slim
 ./scripts/feeds install -a -p slim
+# diskman depends on parted
+mkdir -p package/parted && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+./scripts/feeds update diskman
+./scripts/feeds install -a -p diskman
 cd $SLIM_CFG_TOP_DIR
