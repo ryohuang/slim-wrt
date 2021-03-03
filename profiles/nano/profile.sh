88f4454a43af1425d405db31abba4076ba3d9daf
ryohuang <anyisdad@icloud.com>
2021-03-03 19:31:52 +0800

    Add nano profile

---
+++ profiles/nano/profile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SLIM_CFG_TARGET="x86"
+SLIM_CFG_ARCH="64"
+
+SLIM_CFG_EXCLUDE_PATCH=(\
+                        0002-upnp \
+                        )
+
+echo ${SLIM_CFG_EXCLUDE_PATCH[*]}
+
+# You can add more exclude patches as following lines.
+# SLIM_CFG_EXCLUDE_PATCH=(\
+#                         0002-upnp \
+#                         0003-nf-conntrack-max \
+#                         )
+
+SLIM_FAKE_VERMAGIC="68143adfcb7fc62a239c4be112fe40de"
